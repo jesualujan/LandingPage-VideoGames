ceffc126bbbd6475970e70547c6b27f9ded8f0cc
Jesua Luján <code.masterqro@gmail.com>
2021-11-05 15:59:26 -0600

    creamos nuestro componente Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.scss';
+
+const Header = () => {
+    return (
+      <div className="header">
+          <div className="header__inner container">
+              <div className="header__logo">
+                  <img src="" alt="" />
+                  </div>
+          </div>
+      </div>
+    )
+}
+
+export default Header

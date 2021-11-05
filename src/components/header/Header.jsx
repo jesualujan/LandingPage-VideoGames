24fc4a7122482b0bf3fe00eb7c18d30df2980a78
Jesua Luján <code.masterqro@gmail.com>
2021-11-05 16:21:14 -0600

    Creamos nuestro componente Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import './header.scss';
+import { logo } from '../../assets/images';
 
 const Header = () => {
     return (
       <div className="header">
           <div className="header__inner container">
               <div className="header__logo">
-                  <img src="" alt="" />
+                  <img src={logo} alt="" />
                   </div>
+                  <ul className="header__nav">
+                      <li><a href="#!">vista rápida</a></li>
+                      <li><a href="#!">nuevo</a></li>
+                      <li><a href="#!">campeones</a></li>
+                      <li><a href="#!">soporte</a></li>
+                  </ul>
           </div>
       </div>
     )

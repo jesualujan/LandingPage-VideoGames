0ace96918dac0cc69418cbae68d4be1951328b01
Jesua Luján <code.masterqro@gmail.com>
2021-11-05 15:23:08 -0600

    importamos las imagenes que usaremos

---
+++ src/assets/images.js
@@ -0,0 +1,65 @@
+//* IMPORTAMOS Y EXPORTAMOS LAS IMAGENES Y EL LOGO
+import logo from './img/logo.png'
+import champAshe from './img/Champion_ashe_HP.png'
+import champAhri from './img/Champion_Ahri_HP.png'
+import champGaren from './img/Champion_garen_HP.png'
+import bg1 from './img/bg-1.jpg'
+import bg2 from './img/bg-2.jpg'
+import bg3 from './img/bg-3.jpg'
+import distortion from './img/distortion.png'
+
+import cardAshe from './img/ashe_card.png'
+import cardAhri from './img/ahri_card.png'
+import cardGaren from './img/garen_card.png'
+import cardFortune from './img/fortune_card.png'
+import cardLux from './img/lux_card.png'
+import cardAkali from './img/akali_card.png'
+
+import bgLux from './img/lux-bg.jpg'
+import bgGaren from './img/garen-bg.jpg'
+import bgAhri from './img/ahri-bg.jpg'
+import bgAshe from './img/ashe-bg.jpg'
+import bgFortune from './img/fortune-bg.jpg'
+import bgAkali from './img/akali-bg.jpg'
+
+import bgLargeAshe from './img/ashe-bg-large.jpg'
+import bgLargeAhri from './img/ahri-bg-large.jpg'
+import bgLargeFortune from './img/fortune-bg-large.jpg'
+import bgLargeLux from './img/lux-bg-large.jpg'
+import bgLargeGaren from './img/garen-bg-large.jpg'
+import bgLargeAkali from './img/akali-bg-large.jpg'
+
+import closeIcon from './img/two-sword.png'
+
+import trailerImg from './img/trailer-img.png'
+
+export {
+    logo,
+    champAhri,
+    champAshe,
+    champGaren,
+    bg1,
+    bg2,
+    bg3,
+    distortion,
+    cardAshe,
+    cardAhri,
+    cardGaren,
+    cardLux,
+    cardFortune,
+    cardAkali,
+    bgLux,
+    bgGaren,
+    bgAshe,
+    bgFortune,
+    bgAhri,
+    bgAkali,
+    closeIcon,
+    bgLargeAshe,
+    bgLargeAhri,
+    bgLargeFortune,
+    bgLargeLux,
+    bgLargeGaren,
+    bgLargeAkali,
+    trailerImg
+}

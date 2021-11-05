79f7657b9bf3f14c3b43422c976bbeb96262f1db
Jesua Luján <code.masterqro@gmail.com>
2021-11-05 16:00:09 -0600

    importamos nuestro componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from './components/header/Header'
 import './App.scss'
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
      <h1>hola!</h1>
     </div>
   )

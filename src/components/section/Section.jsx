2dc0ea37440403e8ec0f458b8eff905d343c3d5b
Jesua Luján <code.masterqro@gmail.com>
2022-02-21 15:53:37 -0600

    creamos el componente section

---
+++ src/components/section/Section.jsx
@@ -0,0 +1,24 @@
+import './section.scss'
+
+const Section = props => {
+  return (
+    <div className={`section ${props.className}`}>
+        {props.children}
+    </div>
+  )
+}
+
+const SectionContent = props => {
+    const bgImage = props.bgImage 
+    ? { backgroundImage: `url(${props.bgImage})`} 
+    : {}
+
+    return (
+      <div className={`section__content ${props.className}`}
+        style={bgImage}>
+          {props.children}
+      </div>
+    )
+  }
+
+export default Section

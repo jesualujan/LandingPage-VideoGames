8271a83c2a5dfb00900e8c9e006cb9477d3aa27e
Jesua Luján <code.masterqro@gmail.com>
2022-02-22 13:35:47 -0600

    en welcome usamos hook para hacer que las imagenes cambien por su indice

--- src/components/home-section/welcome/Welcome.jsx
+++ src/components/home-section/welcome/Welcome.jsx
@@ -1,6 +1,9 @@
+import {useEffect} from 'react'
 import './welcome.scss'
 import HomeSection from '../HomeSection'
 import Button from '../../button/Button'
+import hoverEffect from 'hover-effect'
+
 import {
     bg1,champGaren,champAshe,champAhri,distortion
 } from '../../../assets/images' 
@@ -8,6 +11,41 @@ import {
 const champImage=[champGaren,champAshe,champAhri]
 
 const Welcome = props => {
+
+    useEffect(() => {
+        const welcomeImgs = document.querySelectorAll('#welcome__img__slide > img')
+        let animates=[]
+        welcomeImgs.forEach((item, index) => {
+            let nextImg = welcomeImgs[index === welcomeImgs.length - 1 ? 0 : index + 1].getAttribute
+            ('src')
+            let animation= new hoverEffect({
+                parent: document.querySelector('#welcome__img__slide'),
+                intensity: 0.5,
+                image1: item.getAttribute('src'),
+                image2:  nextImg,
+                displacementImage: distortion,
+                hover:false
+            })
+            animates.push(animation)
+        })
+        welcomeImgs.forEach(e=>e.remove())
+        let currentItem=0;
+        const autoImageSlide = () => {
+            let previousItem = currentItem
+            currentItem = (currentItem + 1) % animates.length
+            if(!document.hidden){
+                animates[previousItem].next()
+            }
+            setTimeout(() => {
+                let canvas = document.querySelectorAll('#welcome__img__slide > canvas')
+                document.querySelector('#welcome__img__slide').appendChild(canvas[0])
+               animates[previousItem].previous() 
+            },10000)
+        }
+        setInterval(autoImageSlide, 10000)
+    }, [])
+    // https://www.youtube.com/watch?v=6sp_a1fnkjE minuto 53:50
+
   return (
     <HomeSection 
     className={`welcome ${props.isActive ? 'active' :''}`}
@@ -30,7 +68,7 @@ const Welcome = props => {
           </div>
       </div>
       <div className="welcome__img relative">
-          <div className="welcome__img__slide">
+          <div className="welcome__img__slide"  id="welcome__img__slide">
               {
                   champImage.map((item, index) => 
                  <img src={item} key={index}/>)
